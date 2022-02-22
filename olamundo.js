c9a595fd4d7787805dd2a5c122703bc34046c483
VitoriaDePaiva <vitoriaconsignado@gmail.com>
2022-02-21 22:32:20 -0300

    Aula 01

---
+++ olamundo.js
@@ -0,0 +1,12 @@
+console.log('Ola mundo')
+var a = 2;
+console.log('Esse é o conteúdo de a = ' ,a)
+var b = 3
+console.log('Esse é o resultado de ', a, '+', b , '=', a+b)
+var c = 8
+console.log('Esse é o resultado de ', a, '*', b , '*', c , '=', a*b*c)
+console.log('Esse é o resultado de ', a, '-', b, '-', c, '=', a-b-c)
+let nome = 'Vitoria'
+console.log('Meu nome é ' , nome)
+let idade = '22'
+console.log('Minha idade é ', idade)
